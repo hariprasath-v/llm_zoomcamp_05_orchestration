5054974e7fc8227d97f2be664305d818df69e355
Hariprasath <49398090+hariprasath-v@users.noreply.github.com>
2024-08-19 09:29:30 +0000

    from_codespaces

---
+++ data_chunking.py
@@ -0,0 +1,26 @@
+#Data chunking
+import re
+from typing import Any, Dict, List
+import hashlib
+
+
+@transformer
+def chunk_documents(data: List[Dict[str, Any]], *args, **kwargs):
+    # Generate a unique document ID
+    def generate_document_id(doc):
+        combined = f"{doc['course']}-{doc['question']}-{doc['text'][:10]}"
+        hash_object = hashlib.md5(combined.encode())
+        hash_hex = hash_object.hexdigest()
+        document_id = hash_hex[:8]
+        return document_id
+    documents = []
+    for course_dict in data:
+        for doc in course_dict['documents']:
+            doc['course'] = course_dict['course']
+            #print(doc)
+            #break
+            # previously we used just "id" for document ID
+            doc['document_id'] = generate_document_id(doc)
+            documents.append(doc)
+    print(f'Documents:', len(documents))
+    return [documents]
---
+++ data_retrieval_elasticsearch.py
@@ -0,0 +1,66 @@
+#iterative retrieval
+from typing import Dict, List, Union
+
+import numpy as np
+from elasticsearch import Elasticsearch, exceptions
+
+
+SAMPLE_QUERY = "When is the next cohort?"
+
+
+@data_loader
+def search(*args, **kwargs) -> List[Dict]:
+    
+    
+    
+    connection_string = kwargs.get('connection_string', 'http://localhost:9200')
+    index_name = kwargs.get('index_name', 'documents')
+    
+    
+
+    script_query = {
+        "size": 1,
+        "query": {
+            "bool": {
+                "must": {
+                    "multi_match": {
+                        "query":SAMPLE_QUERY,
+                        "fields": ["question^5", "text", "section"],
+                        "type": "best_fields"
+                    }
+                },
+                "filter": {
+                    "term": {
+                        "course": 'llm-zoomcamp'
+                    }
+                }
+            }
+        }
+    }
+
+
+    
+
+    
+    print("Sending script query:", script_query)
+
+    es_client = Elasticsearch(connection_string)
+    
+    try:
+        response = es_client.search(
+            index=index_name,
+            body= script_query,
+                
+            
+        )
+
+        print("Raw response from Elasticsearch:", response)
+
+        return [hit['_source'] for hit in response['hits']['hits']]
+    
+    except exceptions.BadRequestError as e:
+        print(f"BadRequestError: {e.info}")
+        return []
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        return []
